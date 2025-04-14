9de4a4a7152e2a8bb89cb49192b908757d166a53
Robin Lemmers <38210@ma-web.nl>
2025-04-14 15:03:00 +0200

    sidebars en style

--- socialmedia/main.js
+++ socialmedia/main.js
@@ -19,7 +19,46 @@ function makeposts(data) {
 
     for (let i = 0; i < data.length; i++) {
         let post = document.createElement("li");
-        post.innerHTML = data[i].content;
+        post.classList.add("post");
+
+        let userDate = document.createElement("div");
+        userDate.classList.add("userDate");
+
+        let user = document.createElement("p");
+        user.classList.add("user");
+        user.innerHTML = data[i].user;
+        
+        let date = document.createElement("p");
+        date.classList.add("date");
+        date.innerHTML = data[i].date;
+
+        userDate.appendChild(user);
+        userDate.appendChild(date);
+
+        let content = document.createElement("p");
+        content.classList.add("content");
+        content.innerHTML = data[i].content;
+        
+        let infoBar = document.createElement("div");
+        infoBar.classList.add("infoBar");
+        
+        let comments = document.createElement("p");
+        comments.innerHTML = `C:${data[i].comments}`;
+        
+        let likes = document.createElement("p");
+        likes.innerHTML = `L:${data[i].likes}`;
+        
+        let reposts = document.createElement("p");
+        reposts.innerHTML = `R:${data[i].reposts}`;
+        
+        infoBar.appendChild(comments);
+        infoBar.appendChild(likes);
+        infoBar.appendChild(reposts);
+        
+        post.appendChild(userDate);
+        post.appendChild(content);
+        post.appendChild(infoBar);
+        
         postsList.appendChild(post);
     }
 }
